2b4b89bb2c4f31d614cf993e38b786047b97afd8
Keerthana J <keerthana.jayakumar.kjk@gmail.com>
2026-08-13 19:53:32 +0530

    Connect backend to Neon database

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
 import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+psycopg://",1)
+
+engine = create_engine(DATABASE_URL)
